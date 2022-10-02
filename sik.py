086f098de7d17eb07ba372732152d0a10cc05161
Arlan <101118277+Sniiiks@users.noreply.github.com>
2022-10-02 22:49:50 +0600

    Add files via upload

---
+++ sik.py
@@ -0,0 +1,9 @@
+
+def gug():
+    x=int(input())
+    y=((x**2)-7*x+6)
+    print(y)
+
+gug()
+
+
---
+++ sok.py
@@ -0,0 +1,7 @@
+
+import math
+
+x=int(input())
+y=int(input())
+c=(math.fabs(x)-math.fabs(y)) / (float) (1+math.fabs(x*y))
+
